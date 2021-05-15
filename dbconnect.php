876d85f07292f87a21794a9dc2ac668f6f3f4f06
Evgen <36047161+Evgen47@users.noreply.github.com>
2021-05-15 19:06:14 +0300

    Исправление ошибок (удаление лишнего)

--- dbconnect.php
+++ dbconnect.php
@@ -3,16 +3,3 @@ $dbconnect = mysqli_connect("127.0.0.1:3306", "root", "root", "doingsdone");
 if ($dbconnect == false){
     print('Ошибка подключения: ' . mysqli_connect_error());
 }
-
-$sql = "SELECT headline_project, user_id FROM projects";
-$result = mysqli_query($dbconnect, $sql);
-
-if($result){
-    $projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
-}
-
-$sql = "SELECT title, user_id, project_id, deadline_date FROM tasks WHERE user_id = '$user_id'";
-$result = mysqli_query($dbconnect, $sql);
-if($result){
-    $tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
-}
